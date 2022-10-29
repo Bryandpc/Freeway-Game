a06adb3382eb0d3edc3c68022d590a4d0932ed3a
Bryan Alexandre <100152661+Bryandpc@users.noreply.github.com>
2022-10-29 20:14:01 -0300

    Create asketch.js

---
+++ asketch.js
@@ -0,0 +1,32 @@
+
+function setup() {
+  createCanvas(500, 400);
+  
+  chadsound.loop()
+}
+
+function draw() {
+  background(estrada);
+  mostravaca()
+  showcar()
+  movecar()
+  movevaca()
+  returncar()
+  returnvaca()
+  vecolisao()
+  pontos()
+  sla()
+}
+
+//coloquei o a na frente do sketch.js pra aparecer como primeiro aquivo e ficar mais facil de achar
+
+
+
+
+
+
+
+  
+  
+
+
